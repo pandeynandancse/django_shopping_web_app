9d4edfb1fa1313ebfe488be96874d58976e32279
Nandan Pandey <pandeynandancse@gmail.com>
2020-07-01 10:50:00 +0530

    Update settings.py

--- mac/settings.py
+++ mac/settings.py
@@ -124,3 +124,7 @@ STATIC_URL = '/static/'
 # Managing media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# All settings common to all environments
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
